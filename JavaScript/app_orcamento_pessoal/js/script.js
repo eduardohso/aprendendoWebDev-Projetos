5d52378c34756be7f7a6bc9b24ac6add277af8d5
Eduardo Oliveira <eduardohsoliveira1@gmail.com>
2022-03-10 02:27:38 -0300

    finalizado

--- JavaScript/app_orcamento_pessoal/js/script.js
+++ JavaScript/app_orcamento_pessoal/js/script.js
@@ -38,19 +38,63 @@ class Bd {
 	recuperarTodosRegistros() {
 		//array de despesas
 		let despesas = Array();
-		let id = localstorage.getItem("id");
+		let id = localStorage.getItem("id");
 		//recuperar todas as despesas cadastradas em localStorage
 		for (let i = 1; i <= id; i++) {
 			//recuperar a despesa
-			let despesa = JSON.parse(localStorage.getItem("i"));
+			let despesa = JSON.parse(localStorage.getItem(i));
 			//existe a possibilidade de haver indices que foram pulados/removidos
 			//nestes casos nos vamos pular esses indices
 			if (despesa === null) {
 				continue;
 			}
+			despesa.id = i;
 			despesas.push(despesa);
 		}
 		return despesas;
+	}
+	pesquisar(despesa) {
+		let despesasFiltradas = Array();
+		despesasFiltradas = this.recuperarTodosRegistros();
+
+		//ano
+		if (despesa.ano != "") {
+			despesasFiltradas = despesasFiltradas.filter(
+				(d) => d.ano == despesa.ano
+			);
+
+			//mes
+		}
+		if (despesa.mes != "") {
+			despesasFiltradas = despesasFiltradas.filter(
+				(d) => d.mes == despesa.mes
+			);
+
+			//dia
+		}
+		if (despesa.dia != "") {
+			despesasFiltradas = despesasFiltradas.filter(
+				(d) => d.dia == despesa.dia
+			);
+
+			//tipo
+		}
+		if (despesa.tipo != "") {
+			despesasFiltradas = despesasFiltradas.filter(
+				(d) => d.tipo == despesa.tipo
+			);
+
+			//descricao
+		}
+		if (despesa.descricao != "") {
+			despesasFiltradas = despesasFiltradas.filter(
+				(d) => d.descricao == despesa.descricao
+			);
+		}
+		return despesasFiltradas;
+	}
+	remover(id) {
+		localStorage.removeItem(id);
 	}
 }
 
@@ -86,6 +130,13 @@ function cadastrarDespesa() {
 		document.getElementById("modal-btn").className = "btn btn-success";
 
 		$("#modalRegistraDespesa").modal("show");
+
+		(ano.value = ""),
+			(mes.value = ""),
+			(dia.value = ""),
+			(tipo.value = ""),
+			(descricao.value = ""),
+			(valor.value = "");
 	} else {
 		document.getElementById("modal-title").innerHTML =
 			"Erro na inclusao do registro";
@@ -100,8 +151,61 @@ function cadastrarDespesa() {
 	}
 }
 
-function carregaListaDespesas() {
-	let despesas = Array();
-	despesas = bd.recuperarTodosRegistros();
-	console.log(despesas);
+function carregaListaDespesas(despesas = Array(), filtro = false) {
+	if (despesas.length == 0 && filtro == false) {
+		despesas = bd.recuperarTodosRegistros();
+	}
+	let listaDespesas = document.getElementById("listaDespesas");
+	listaDespesas.innerHTML = "";
+
+	despesas.forEach(function (d) {
+		let linha = listaDespesas.insertRow();
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+		switch (d.tipo) {
+			case "1":
+				d.tipo = "Alimentação";
+				break;
+			case "2":
+				d.tipo = "Educação";
+				break;
+			case "3":
+				d.tipo = "Lazer";
+				break;
+			case "4":
+				d.tipo = "Saúde";
+				break;
+			case "5":
+				d.tipo = "Transporte";
+				break;
+		}
+		linha.insertCell(1).innerHTML = d.tipo;
+		linha.insertCell(2).innerHTML = d.descricao;
+		linha.insertCell(3).innerHTML = d.valor;
+
+		//botao exclusao
+		let btn = document.createElement("button");
+		btn.className = "btn btn-danger";
+		btn.innerHTML = '<i class="fas fa-times"></i>';
+		btn.id = `id_despesa_${d.id}`;
+		btn.onclick = function () {
+			let id = this.id.replace("id_despesa_", "");
+			bd.remover(id);
+			window.location.reload();
+		};
+		linha.insertCell(4).append(btn);
+	});
+}
+function pesquisarDespesa() {
+	let ano = document.getElementById("ano").value;
+	let mes = document.getElementById("mes").value;
+	let dia = document.getElementById("dia").value;
+	let tipo = document.getElementById("tipo").value;
+	let descricao = document.getElementById("descricao").value;
+	let valor = document.getElementById("valor").value;
+
+	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
+
+	let despesas = bd.pesquisar(despesa);
+
+	carregaListaDespesas(despesas, true);
 }
